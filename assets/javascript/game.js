02b720da4c05ed1925b5f2432e02d1990ae7b7cb
Nnamdi Iroham <niroham@yahoo.com>
2019-09-27 11:43:26 -0500

    added append and function keys to enable my choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,116 +1,125 @@
-const questions = [
-    {
-        questions: "Which Greek philospher believed that wisdom comes from accepting there is much we don't know?",
-        choices: ["Plato", "Pythagoras", "Socrates", "Aristotle"],
-        correctAnswer: "Socrates"
-    },
+$(document).ready(function () {
 
-    {
-        questions: "Who directed the epic film Clockwork Orange?",
-        choices: ["Tarantino", "Kubrick", "King", "Scorsese"],
-        correctAnswer: "Kubrick"
-    },
+    const questions = [
+        {
+            questions: "Which Greek philospher believed that wisdom comes from accepting there is much we don't know?",
+            choices: ["Plato", "Pythagoras", "Socrates", "Aristotle"],
+            correctAnswer: "Socrates"
+        },
 
-    {
-        questions: "Who created the hit TV-show Simpsons?",
-        choices: ["Giligan", "Groening", "Simon", "Chase"],
-        correctAnswer: "Groening"
-    },
+        {
+            questions: "Who directed the epic film Clockwork Orange?",
+            choices: ["Tarantino", "Kubrick", "King", "Scorsese"],
+            correctAnswer: "Kubrick"
+        },
 
-    {
+        {
+            questions: "Who created the hit TV-show Simpsons?",
+            choices: ["Giligan", "Groening", "Simon", "Chase"],
+            correctAnswer: "Groening"
+        },
 
-        questions: "In what country is the city of Petra found?",
-        choices: ["Syria", "China", "Jordan", "Egypt"],
-        correctAnswer: "Jordan"
-    },
+        {
 
-    {
-        questions: "Which classical genius composed The Nutcracker op. 71, TH 14/Act 2-No 12e Divertissement ?",
-        choices: ["Bach", "Mozart", "Tchaikovsky", "Wagner"],
-        correctAnswer: "Tchaikovsky"
-    }
-]
+            questions: "In what country is the city of Petra found?",
+            choices: ["Syria", "China", "Jordan", "Egypt"],
+            correctAnswer: "Jordan"
+        },
 
-
-//initial values
-let counter = 30;
-let currentQuestion = 0;
-let score = 0;
-let lost = 0;
-let timer;
-
-function nextQuestion() {
-
-    const isQuestionOver = (questions.length - 1) === currentQuestion;
-    if (isQuestionOver) {
-        console.log('Game is over!!');
-    } else {
-
-        currentQuestion++;
-        loadQuestion();
-    }
-
-}
-
-function timeUp() {
-    clearInterval();
-
-    lost++;
-    nextQuestion();
-}
-// start a 30 second timer
-function countDown() {
-    counter--;
-
-    $('#time').html('Timer: ' + counter);
-
-    if (counter === 0) {
-        timeUp();
-    }
-}
+        {
+            questions: "Which classical genius composed The Nutcracker op. 71, TH 14/Act 2-No 12e Divertissement ?",
+            choices: ["Bach", "Mozart", "Tchaikovsky", "Wagner"],
+            correctAnswer: "Tchaikovsky"
+        }
+    ]
 
 
-//Display questions and answers on the browser
+    //initial values
+    let counter = 30;
+    let currentQuestion = 0;
+    let score = 0;
+    let lost = 0;
+    let timer;
 
-function loadQuestion() {
-    counter = 30;
-    timer = setInterval(countDown, 1000);
+    function nextQuestion() {
 
-    const questions = questions[currentQuestion].questions; //
-    const choices = questions[currentQuestion].choices; //
+        const isQuestionOver = (questions.length - 1) === currentQuestion;
+        $(".choices").empty();
+        if (isQuestionOver) {
+            console.log('Game is over!!');
+        } else {
 
-    $("#time").html('Timer:' + counter);
-    $("#game").html(
-        <h4>${questions}</h4>
-        ${ "loadChoices"(choices) }
-    );
-}
+            currentQuestion++;
+            loadQuestion();
+        }
 
-function loadChoices(choices) {
-    let result = '';
 
-    for (let i = 0; i < choices.length; i++) {
-        result += '<p class="choices" data-answer="${choices[i]}">${choices[i]}</p>'
     }
 
-    return result;
-}
+    function timeUp() {
+        clearInterval();
 
-// Either pick right answer or move on to next question
-
-$(document).on('click', '.choice', function () {
-    const selectedAnswer = $(this).attr('data-answer');
-    const correctAnswer = questions[currentQuestion].correctAnswer;
-    
-    if (correctAnswer === selectedAnswer) {
-        score++;
-        console.log('Wins!!')
-    } else {
         lost++;
-        console.log('lost!!')
+        $(".choices").empty();
+        nextQuestion();
     }
-    
-    console.log('Yippie!!', selectedAnswer);
-});;
+    // start a 30 second timer
+    function countDown() {
+        counter--;
+
+        $('#time').html('Timer: ' + counter);
+
+        if (counter === 0) {
+            timeUp();
+        }
+    }
 
 
+    //Display questions and answers on the browser
+    // $()
+    //$("h4")
+
+
+    function loadQuestion() {
+        counter = 30;
+        timer = setInterval(countDown, 1000);
+
+        const question = questions[currentQuestion].questions; //
+
+        $("#time").html('Timer:' + counter);
+        $("#quest").html(question);
+        loadChoices();
+    }
+
+    function loadChoices() {
+        const choices = questions[currentQuestion].choices; //
+
+        for (let i = 0; i < choices.length; i++) {
+            var choiceButton = $("<button class='btn btn-primary choice' data-answer='" + choices[i] + "'>" + choices[i] + "</button>")
+            $(".choices").append(choiceButton)
+        }
+
+    }
+
+    // Either pick right answer or move on to next question
+
+    $(document).on('click', '.choice', function () {
+        const selectedAnswer = $(this).attr('data-answer');
+        const correctAnswer = questions[currentQuestion].correctAnswer;
+
+        if (correctAnswer === selectedAnswer) {
+            score++;
+            alert("You win! " + selectedAnswer + " was the correct answer.")
+        } else {
+            lost++;
+            alert("You lose! You chose " + selectedAnswer + " but the correct answer was " + correctAnswer)
+        }
+
+        console.log('Yippie!!', selectedAnswer);
+        nextQuestion()
+    });;
+
+    $(".choices").empty();
+    loadQuestion();
+
+});
